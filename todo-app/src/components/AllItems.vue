7e38e359902bb364eee2eb6fb2988db8d1ffed59
Marshall88GB <gasic.boban@gmail.com>
2020-08-09 10:10:06 +0200

    first commit

---
+++ todo-app/src/components/AllItems.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <item
+      v-for="(item) in todo.items"
+      v-bind:key="item.id"
+      v-bind:item="item"
+      v-on:item-delete="onDelete(item)"
+    ></item>
+  </div>
+</template>
+
+<script>
+import Item from "./Item";
+
+export default {
+  name: "AllItems",
+  props: ["todo"],
+  components: {
+    item: Item
+  },
+  methods: {
+    onDelete: function(item) {
+      this.todo.delete(item);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style> 
---
+++ todo-app/src/components/Item.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todo-item"  >
+    <div class="todo-item-content">
+      <span class="data">{{item.content}}</span>
+    </div>
+    <span class="delete-btn" title="delete" v-on:click.stop="onDelete"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  props: ["item"],
+  methods: {
+    onDelete: function() {
+      this.$emit("item-delete");
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
